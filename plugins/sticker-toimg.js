5e81dd1dd5d9ac96243a1b903b33fc1a9a70300e
mohamedSharkawy1598 <124299658+mohamedSharkawy1598@users.noreply.github.com>
2023-05-06 00:01:03 +0300

    Update sticker-toimg.js

--- plugins/sticker-toimg.js
+++ plugins/sticker-toimg.js
@@ -1,17 +1,17 @@
 import { webp2png } from '../lib/webp2mp4.js'
 
 let handler = async (m, { conn, usedPrefix, command }) => {
-    const notStickerMessage = `✳️ Responde a un sticker con :\n\n *${usedPrefix + command}*`
+    const notStickerMessage = `*『✨🐸قم برد علي ملصق لي تحويله لي صورة』*`
     if (!m.quoted) throw notStickerMessage
     const q = m.quoted || m
     let mime = q.mediaType || ''
     if (!/sticker/.test(mime)) throw notStickerMessage
     let media = await q.download()
     let out = await webp2png(media).catch(_ => null) || Buffer.alloc(0)
-    await conn.sendFile(m.chat, out, 'out.png', '*✅ Aquí tienes*', m)
+    await conn.sendFile(m.chat, out, 'out.png', '*『تم تحويل الملصق بنجاح لصورة🐸▶️』*', m)
 }
 handler.help = ['toimg <sticker>']
 handler.tags = ['sticker']
-handler.command = ['toimg', 'jpg', 'aimg'] 
+handler.command = ['toimg', 'jpg', 'لصورة'] 
 
 export default handler
