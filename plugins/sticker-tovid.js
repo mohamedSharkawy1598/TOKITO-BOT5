206933a14975235dcb1ffe0ba247deec8eae0bf3
mohamedSharkawy1598 <124299658+mohamedSharkawy1598@users.noreply.github.com>
2023-05-05 23:47:04 +0300

    Update sticker-tovid.js

--- plugins/sticker-tovid.js
+++ plugins/sticker-tovid.js
@@ -4,7 +4,7 @@ import { ffmpeg } from '../lib/converter.js'
 let handler = async (m, { conn }) => {
     if (!m.quoted) throw '  *『✨🐸قم برد علي ملصق فيديو لي تحويله لي فيديو』*'
     let mime = m.quoted.mimetype || ''
-    if (!/webp|audio/.test(mime)) throw '✳️ Responde a un sticker animado'
+    if (!/webp|audio/.test(mime)) throw '*『✨🐸قم برد علي ملصق فيديو لي تحويله لي فيديو』*'
     let media = await m.quoted.download()
     let out = Buffer.alloc(0)
     if (/webp/.test(mime)) {
@@ -18,7 +18,7 @@ let handler = async (m, { conn }) => {
             '-shortest'
         ], 'mp3', 'mp4')
     }
-    await conn.sendFile(m.chat, out, 'tovid.mp4', '✅ sticker a video' , m)
+    await conn.sendFile(m.chat, out, 'tovid.mp4', '*『تم تحويل الملصق بنجاح لفيديو🐸▶️』*' , m)
 }
 handler.help = ['tovid']
 handler.tags = ['sticker']
