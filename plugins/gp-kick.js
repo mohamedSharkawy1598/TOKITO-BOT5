16ffce050833f80ae01aba973fc42d250de0348f
mohamedSharkawy1598 <124299658+mohamedSharkawy1598@users.noreply.github.com>
2023-05-06 18:11:03 +0300

    Update gp-kick.js

--- plugins/gp-kick.js
+++ plugins/gp-kick.js
@@ -1,19 +1,19 @@
 
 let handler = async (m, { conn, participants, usedPrefix, command }) => {
 	
-let kickte = `✳️ Uso correcto del comamdo\n*${usedPrefix + command}* @tag`
+let kickte = `*『منشن احد لي طرده او قم برد علي الاشارة له له✨🙂』*`
 
 if (!m.mentionedJid[0] && !m.quoted) return m.reply(kickte, m.chat, { mentions: conn.parseMention(kickte)}) 
 let user = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted.sender
 let owr = m.chat.split`-`[0]
 await conn.groupParticipantsUpdate(m.chat, [user], 'remove')
-m.reply(`✅ Usuario eliminado`) 
+m.reply(`*✅ تم طرد بنجاح*`) 
 
 }
 
 handler.help = ['kick @user']
 handler.tags = ['group']
-handler.command = ['kick', 'expulsar'] 
+handler.command = ['طرد', 'اطردو'] 
 handler.admin = true
 handler.group = true
 handler.botAdmin = true
