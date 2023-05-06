01482ac9a46c0c454dadc4158110ccf0114992b5
dazi1232 <132753658+dazi1232@users.noreply.github.com>
2023-05-06 18:26:12 +0300

    Update anime-rslap.js

--- plugins/anime-rslap.js
+++ plugins/anime-rslap.js
@@ -7,7 +7,7 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
 	 let who
     if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : false
     else who = m.chat
-     if (!who) throw `✳️ Etiqueta o menciona a alguien\n\n📌 Ejemplo : ${usedPrefix + command} @tag`
+     if (!who) throw `✳️ Etiqueta o menciona a alguien\n\n📌 مثال : ${usedPrefix + command} @tag`
      
     let user = global.db.data.users[who]
     let name = conn.getName(who) 
@@ -26,7 +26,7 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
 
 handler.help = ['slap @tag']
 handler.tags = ['rnime']
-handler.command = /^(slap|bofetada)$/i
+handler.command = /^(نوم|bofetada)$/i
 handler.diamond = true
 handler.group = true
 
