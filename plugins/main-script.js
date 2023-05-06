b92d351138b8792dc63590bffa777ee18b9236ad
dazi1232 <132753658+dazi1232@users.noreply.github.com>
2023-05-06 19:02:18 +0300

    Update main-script.js

--- plugins/main-script.js
+++ plugins/main-script.js
@@ -5,15 +5,12 @@ let handler = async function (m, { conn, __dirname }) {
 let _package = JSON.parse(await promises.readFile(join(__dirname, '../package.json')).catch(_ => ({}))) || {}
   
 m.reply(`
-*≡ SCRIPT*
-
-▢ Git : ${_package.homepage}
-`.trim())
+*لاتحاول حبيبي
     
 }
 
 handler.help = ['script']
 handler.tags = ['main']
-handler.command = ['sc', 'git', 'script'] 
+handler.command = ['sc', 'سكربت', 'script'] 
 
 export default handler
