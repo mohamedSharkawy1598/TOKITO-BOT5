41433766d08ecdc901447517ba5c404cb7cf0dea
dazi1232 <132753658+dazi1232@users.noreply.github.com>
2023-05-06 15:36:08 +0300

    Update fun-tomp3.js

--- plugins/fun-tomp3.js
+++ plugins/fun-tomp3.js
@@ -4,7 +4,7 @@ let handler = async (m, { conn, usedPrefix, command }) => {
     try {
     let q = m.quoted ? m.quoted : m
    let mime = (m.quoted ? m.quoted : m.msg).mimetype || ''
-   // if (!/video|audio/.test(mime)) throw `✳️ Responda al video o nota de voz que desea convertir a mp3 con el comando :\n\n*${usedPrefix + command}*`
+   // if (!/video|audio/.test(mime)) throw `✳️ قم بالرد على الفيديو أو الملاحظة الصوتية التي تريد تحويلها إلى لصوت :\n\n*${usedPrefix + command}*`
     let media = await q.download?.()
     if (!media) throw '❎ Error al descargar medios'
     let audio = await toAudio(media, 'mp4')
